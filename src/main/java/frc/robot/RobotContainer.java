e60852b93b5d718623b24444d23143866a06a666
FalseLogic <firealarm02@gmail.com>
2022-02-15 16:08:30 -0500

    added drivetrain, stick, and defaultcommands

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -5,14 +5,32 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.commands.defaultcommands.DefaultArcadeDrive;
+import frc.robot.subsystems.Drivetrain;
 
 public class RobotContainer {
 
+  private Drivetrain drivetrain;
+  private Joystick stick;
+
   public RobotContainer() {
 
+    drivetrain = new Drivetrain();
+
+    stick = new Joystick(0);
+
+    setDefaultCommands();
     configureButtonBindings();
+  }
+
+  private void setDefaultCommands() {
+    drivetrain.setDefaultCommand(new DefaultArcadeDrive(() -> stick.getY(), 
+                                                        () -> stick.getTwist(),
+                                                        () -> stick.getTrigger(),
+                                                        drivetrain));
   }
 
   private void configureButtonBindings() {}
