6fdad610d145db6992313ef615206f3f68716401
ice-blokk <anthonythomastan@gmail.com>
2022-02-22 10:53:04 -0500

    built out transport subsystem

--- src/main/java/frc/robot/subsystems/Transport.java
+++ src/main/java/frc/robot/subsystems/Transport.java
@@ -4,14 +4,55 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
 
 public class Transport extends SubsystemBase {
 
-  public Transport() {}
+  private TalonSRX transport;
+  private DigitalInput beamBreak;
+
+  private double startEncoderValue;
+
+  public Transport() {
+    transport = new TalonSRX(Constants.TRANSPORT_ADDRESS);
+
+    beamBreak = new DigitalInput(Constants.TRANSPORT_BEAM_BREAK_ADDRESS);
+
+    transport.setNeutralMode(NeutralMode.Brake);
+    transport.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative);
+  }
+
+  public void setFeeder(double power) {
+    transport.set(ControlMode.PercentOutput, power);
+  }
+
+  public double getEncoderValue() {
+    return transport.getSelectedSensorPosition();
+  }
+
+  public boolean getBeamBreak() {
+    return beamBreak.get();
+  }
+
+  public double getEncoderDelta() {
+    return Math.abs(startEncoderValue - transport.getSelectedSensorPosition());
+  }
+
+  public void resetEncoderDelta() {
+    startEncoderValue = transport.getSelectedSensorPosition();
+  }
 
   @Override
   public void periodic() {
-
+    SmartDashboard.putNumber("Transport Encoder", transport.getSelectedSensorPosition());
+    SmartDashboard.putNumber("Transport Encoder Delta", getEncoderDelta());
   }
 }
