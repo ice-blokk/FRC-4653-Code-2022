af31bfe2c480554ee50ad4fc591597dc341ad04a
ice-blokk <anthonythomastan@gmail.com>
2022-02-22 10:52:41 -0500

    shoot lmao

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -4,9 +4,13 @@
 
 package frc.robot.subsystems;
 
+import com.revrobotics.CANSparkMax;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Shooter extends SubsystemBase {
+
+  private CANSparkMax leadShooter, followerShooter;
 
   public Shooter() {}
 
