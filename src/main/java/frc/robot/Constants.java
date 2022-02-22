a8188048b301aa43da2111eaf7c64aa9f8cfb248
ice-blokk <anthonythomastan@gmail.com>
2022-02-22 10:51:30 -0500

    more constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -16,24 +16,38 @@ public final class Constants {
     public static final int INTAKE_ADDRESS = 99, // talon
                             ARM_ADDRESS = 99; // talon
 
+    public static final double kArmUpPosition = 99,
+                               kArmDownPosition = 99;
+
     // Turret constants
     public static final int TURRET_ADDRESS = 99, // talon
                             TURRET_FWD_HALL_EFFECT = 99,
                             TURRET_REV_HALL_EFFECT = 99,
 
                             kTurretSlotIdx = 0,
-                            kTurretPIDLoopIdx = 0,
+                            kTurretPIDLoopIdx = 0;
+ 
+    public static final double  kTurretP = 99,
+                                kTurretI = 99,
+                                kTurretD = 99,
+                                kTurretF = 99,
 
-                            kTurretP = 99,
-                            kTurretI = 99,
-                            kTurretD = 99,
-                            kTurretF = 99,
+                                kSoftMaxTurretAngle = 99,
+                                kSoftMinTurretAngle = 99,
 
-                            kSoftMaxTurretAngle = 99,
-                            kSoftMinTurretAngle = 99,
-                            kTurretRotationsPerTick = 99;
+                                kTurretSpurGearRatio = 149.0 / 10.0, // ratio of gear attached to turret to gear on motor
+                                kTurretMotorGearRatio = 45.0 / 1.0, // ratio of motor with gearbox
+                                kVersaPlanetaryEncoderCountsPerRevolution = 1024, // CPS of encoder on motor
+                                kTurretRotationsPerTick = (1.0 / kVersaPlanetaryEncoderCountsPerRevolution) * (1.0 / kTurretMotorGearRatio) / (1.0 / kTurretSpurGearRatio); 
+                                // i have no idea of the above calculation is at all correct
 
+    // Transport constants
+    public static final int TRANSPORT_ADDRESS = 99,
+                            TRANSPORT_BEAM_BREAK_ADDRESS = 99;
 
+    
+
+    public static final double kLimelightAngle = 999; // from the horizontal direction
 
     // Odometry + Trajectory Constants
 
