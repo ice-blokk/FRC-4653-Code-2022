77f29c25a1c3b4770fac99ec7c1e27eb74bf4e3c
ice-blokk <anthonythomastan@gmail.com>
2022-02-22 10:52:06 -0500

    created default transport command

---
+++ src/main/java/frc/robot/commands/defaultcommands/DefaultFeedTransport.java
@@ -0,0 +1,68 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands.defaultcommands;
+
+import java.util.function.BooleanSupplier;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.Transport;
+
+public class DefaultFeedTransport extends CommandBase {
+  
+  private Transport transport;
+
+  private BooleanSupplier up, down;
+  private Boolean sensorWasTriggered = false;
+
+  public DefaultFeedTransport(BooleanSupplier up, BooleanSupplier down, Transport transport) {
+    this.transport = transport;
+    addRequirements(transport);
+
+    this.up = up;
+    this.down = down;
+  }
+
+
+  @Override
+  public void initialize() {}
+
+  
+  @Override
+  public void execute() {
+    if(up.getAsBoolean()) {
+      transport.setFeeder(-1);
+    }
+    else if(down.getAsBoolean()) {
+      transport.setFeeder(1);
+    }
+    else {
+
+      if(transport.getBeamBreak()) {
+        transport.resetEncoderDelta();
+        transport.setFeeder(-1);
+        sensorWasTriggered = true;
+      }
+      else if(sensorWasTriggered) {
+        if(transport.getEncoderDelta() > 999) { // TODO: get transport encoder values
+          transport.setFeeder(0);
+          transport.resetEncoderDelta();
+          sensorWasTriggered = false;
+        }
+      
+      }
+    } // end of main if statement
+  } // end of execute()
+
+  
+  @Override
+  public void end(boolean interrupted) {
+  }
+
+  
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
