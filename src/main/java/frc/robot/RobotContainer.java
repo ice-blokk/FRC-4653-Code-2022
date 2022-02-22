b94c3c555b2e2a41f0d0b0ad59919e49c62f69db
ice-blokk <anthonythomastan@gmail.com>
2022-02-22 10:51:48 -0500

    added transport and xbox controller

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -9,28 +9,38 @@ import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.commands.defaultcommands.DefaultArcadeDrive;
+import frc.robot.commands.defaultcommands.DefaultFeedTransport;
 import frc.robot.subsystems.Drivetrain;
+import frc.robot.subsystems.Transport;
 
 public class RobotContainer {
 
   private Drivetrain drivetrain;
+  private Transport transport;
+  
   private Joystick stick;
+  private XboxController xbox;
 
   public RobotContainer() {
 
     drivetrain = new Drivetrain();
+    transport = new Transport();
 
     stick = new Joystick(0);
+    xbox = new XboxController(1);
 
     setDefaultCommands();
     configureButtonBindings();
   }
 
   private void setDefaultCommands() {
-    drivetrain.setDefaultCommand(new DefaultArcadeDrive(() -> stick.getY(), 
-                                                        () -> stick.getTwist(),
-                                                        () -> stick.getTrigger(),
+    drivetrain.setDefaultCommand(new DefaultArcadeDrive(() -> stick.getY(), // speed
+                                                        () -> stick.getTwist(), // rotation
+                                                        () -> stick.getTrigger(), // invert
                                                         drivetrain));
+    transport.setDefaultCommand(new DefaultFeedTransport(() -> xbox.getStartButton(), // up
+                                                         () -> xbox.getBackButton(), // down
+                                                         transport));
   }
 
   private void configureButtonBindings() {}
