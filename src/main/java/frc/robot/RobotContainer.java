7e9e0bd37209d9a95a7c4bdde861385483a5e266
ice-blokk <anthonythomastan@gmail.com>
2022-02-23 11:04:40 -0500

    added intake and chooser

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -7,30 +7,40 @@ package frc.robot;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.commands.defaultcommands.DefaultArcadeDrive;
 import frc.robot.commands.defaultcommands.DefaultFeedTransport;
+import frc.robot.commands.defaultcommands.DefaultIntake;
 import frc.robot.subsystems.Drivetrain;
+import frc.robot.subsystems.Intake;
 import frc.robot.subsystems.Transport;
 
 public class RobotContainer {
 
   private Drivetrain drivetrain;
   private Transport transport;
+  private Intake intake;
   
   private Joystick stick;
   private XboxController xbox;
+
+  private SendableChooser<Command> chooser;
 
   public RobotContainer() {
 
     drivetrain = new Drivetrain();
     transport = new Transport();
+    intake = new Intake();
 
     stick = new Joystick(0);
     xbox = new XboxController(1);
 
+    chooser = new SendableChooser<Command>();
+
     setDefaultCommands();
     configureButtonBindings();
+    initializeAutoChooser();
   }
 
   private void setDefaultCommands() {
@@ -38,14 +48,25 @@ public class RobotContainer {
                                                         () -> stick.getTwist(), // rotation
                                                         () -> stick.getTrigger(), // invert
                                                         drivetrain));
+    
     transport.setDefaultCommand(new DefaultFeedTransport(() -> xbox.getStartButton(), // up
                                                          () -> xbox.getBackButton(), // down
                                                          transport));
+    
+    intake.setDefaultCommand(new DefaultIntake(() -> xbox.getXButton(), // in 
+                                               () -> xbox.getBButton(), // out
+                                               () -> xbox.getYButton(), // arm up
+                                               () -> xbox.getAButton(), // arm down
+                                               intake));
   }
 
   private void configureButtonBindings() {}
 
+  private void initializeAutoChooser() {
+    chooser.setDefaultOption("Nothing", null);
+  }
+
   public Command getAutonomousCommand() {
-    return null;
+    return chooser.getSelected();
   }
 }
