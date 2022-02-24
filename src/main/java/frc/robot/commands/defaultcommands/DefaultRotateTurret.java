bd16eed0b4344d8d2f3956f901479a4b5b14b347
ice-blokk <anthonythomastan@gmail.com>
2022-02-24 11:27:02 -0500

    created turret command

---
+++ src/main/java/frc/robot/commands/defaultcommands/DefaultRotateTurret.java
@@ -0,0 +1,79 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands.defaultcommands;
+
+import java.util.function.BooleanSupplier;
+import java.util.function.DoubleSupplier;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.Turret;
+import frc.robot.util.Limelight;
+
+public class DefaultRotateTurret extends CommandBase {
+
+  private Turret turret;
+  private Limelight limelight;
+
+  private PIDController pid;
+
+  private DoubleSupplier rotate;
+  private BooleanSupplier target;
+
+  private double turretTargetPower;
+
+  public DefaultRotateTurret(DoubleSupplier rotate, BooleanSupplier target, Limelight limelight, Turret turret) {
+    this.turret = turret;
+    this.rotate = rotate;
+    this.target = target;
+    this.limelight = limelight;
+
+    pid = new PIDController(.0002, .00001, 0);
+    pid.setSetpoint(0);
+
+    addRequirements(turret);
+  }
+
+
+  @Override
+  public void initialize() {}
+
+
+  @Override
+  public void execute() {
+    
+    if(Math.abs(rotate.getAsDouble()) > 0) {
+      turret.setOpenLoop(rotate.getAsDouble() * -1);
+    }
+    else if(target.getAsBoolean()) {
+      
+      turretTargetPower = pid.calculate(limelight.getX());
+      
+      if(turretTargetPower > 1) {
+        turretTargetPower = 1;
+      }
+      else if(turretTargetPower < -1) {
+        turretTargetPower = -1;
+      }
+
+      turret.setOpenLoop(turretTargetPower);
+
+    }
+    else {
+      turret.setOpenLoop(0);
+    }
+  
+  } // end of execute
+
+  @Override
+  public void end(boolean interrupted) {
+    turret.setOpenLoop(0);
+  }
+
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
--- src/main/java/frc/robot/subsystems/Turret.java
+++ src/main/java/frc/robot/subsystems/Turret.java
@@ -5,6 +5,7 @@
 package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.DemandType;
 import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 import com.ctre.phoenix.motorcontrol.LimitSwitchNormal;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
