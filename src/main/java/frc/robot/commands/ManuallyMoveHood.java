fdc1dedc49831870549468b0a4b07a182c8e90a2
ice-blokk <anthonythomastan@gmail.com>
2022-03-09 21:26:07 -0500

    created command to manually move hood

--- src/main/java/frc/robot/commands/ManuallyMoveHood.java
+++ src/main/java/frc/robot/commands/ManuallyMoveHood.java
@@ -4,21 +4,40 @@
 
 package frc.robot.commands;
 
+import java.util.function.BooleanSupplier;
+
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.Shooter;
 
 public class ManuallyMoveHood extends CommandBase {
-  /** Creates a new ManuallyMoveHood. */
-  public ManuallyMoveHood() {
-    // Use addRequirements() here to declare subsystem dependencies.
+  private Shooter shooter;
+  private BooleanSupplier up, down;
+  private double position, increment;
+
+  public ManuallyMoveHood(BooleanSupplier up, BooleanSupplier down, Shooter shooter) {
+    this.shooter = shooter;
+    this.up = up;
+    this.down = down;
+    addRequirements(shooter);
   }
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+    position = 100;
+    increment = .1;
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void execute() {
+    if(up.getAsBoolean()) {
+      position += increment;
+    }
+    if(down.getAsBoolean()) {
+      position -= increment;
+    }
+  }
 
   // Called once the command ends or is interrupted.
   @Override
